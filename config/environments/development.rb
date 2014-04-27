df38f53cbfc3d28d97893cd4c1aeda26499c12fa
Kyle Chadha <kyle.chadha@gmail.com>
2014-04-27 10:45:48 -0400

    Configure Guard & LiveReload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,8 @@ TwitterExplorer::Application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  # Use Rack::LiveReload
+  config.middleware.use Rack::LiveReload
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large

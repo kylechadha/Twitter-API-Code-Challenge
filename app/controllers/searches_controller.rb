4fbc1715c544c88273f35956288f081ffdad9f82
Kyle Chadha <kyle.chadha@gmail.com>
2014-04-27 19:49:33 -0400

    Initialize Foundation; Construct search index view

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,7 @@
 class SearchesController < ApplicationController
   def index
     @results = Search.request("monkeys")
-    
   end
-
-
 
   def new
   end
--- app/models/search.rb
+++ app/models/search.rb
@@ -2,9 +2,10 @@ class Search
   include Mongoid::Document
 
   def self.request(keywords)
-    # Search for the keywords and return ID of the top result
     # search_results = 
 
+    # current_user.
+    # authorization_request = 
 
     HTTParty.get "https://api.twitter.com/1.1/search/tweets.json", 
       {
@@ -12,19 +13,10 @@ class Search
           q: keywords
         },
         headers: {
-          "Authorization" => 'OAuth oauth_consumer_key="900kdPyvEBSWxul2mPFWi4BdT", oauth_nonce="379aabe6b773fc995aaf276995ef73b8", oauth_signature="ZCjlSq%2FGZ2bPV%2BfuwrGbPtcBmYI%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1398635970", oauth_token="1537159381-ZbqzUQ34orD7szZH52iywvov7DD7zqYjmwEwjWx", oauth_version="1.0"'
+          "Authorization" => 'OAuth oauth_consumer_key="900kdPyvEBSWxul2mPFWi4BdT", oauth_nonce="7a939598cd1b29bae976545e9c38194b", oauth_signature="m%2Bu4nACfEqDFBvaJCSaHdEDbOmk%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1398640493", oauth_token="1537159381-ZbqzUQ34orD7szZH52iywvov7DD7zqYjmwEwjWx", oauth_version="1.0"'
         }
       }
 
-
-
-    # search_results = HTTParty.get "https://api.twitter.com/1.1/search/tweets.json?q=%40twitterapi"
-
-
-    # movie_id = search_results['movies'][0]['id']
-
-    #Grab the movie data from the ID
-    # HTTParty.get "http://api.rottentomatoes.com/api/public/v1.0/movies/#{movie_id}.json?apikey=k2mrwxaafkd7p8z3zvyp6jcp"
   end
 
 end

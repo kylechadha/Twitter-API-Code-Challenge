1031e429649b8c096d774800914a292307103ea1
Kyle Chadha <kyle.chadha@gmail.com>
2014-04-30 17:08:26 -0400

    Button based infinite scroll

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,13 +3,11 @@ class SearchesController < ApplicationController
     @display = 10
     if params[:increment]
       @display += params[:increment].to_i
-      # binding.pry
     end
 
     if params[:search]
       @results = current_user.twitter.search(params[:search], :count => @display)
     end
-    # binding.pry
 
     respond_to do |format|
       format.html
